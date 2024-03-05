9c1b5376d216666f993aec035abaa68c23253ce4
medsar21 <mohamed12sar@gmail.com>
2024-03-05 16:26:47 +0100

    configurer la partie frontend  template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/',[TemplateController::class,'index']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
